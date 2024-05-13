64b3b052fe33d5d5abebaafb2159388e69ec0b5f
Ubuntu <ubuntu@ip-172-31-37-75.ap-southeast-1.compute.internal>
2024-05-13 13:56:07 +0000

    Init

---
+++ install.sh
@@ -0,0 +1,28 @@
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+export PATH="$PATH:/opt/nvim-linux64/bin"
+
+rm nvim-linux64.tar.gz
+
+sudo apt install ubuntu-drivers-common
+sudo ubuntu-drivers list --gpgpu
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-4
+
+sudo apt-get install -y cuda-drivers
+
+# Install python3
+sudo apt install python3
+sudo apt update
+sudo apt install python3-pip
+
+# Install conda
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
