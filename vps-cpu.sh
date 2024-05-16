1fe7e01612083af0614e2113e45d39ce1673e289
Loc Le <lpl212757@gmail.com>
2024-05-16 03:48:31 +0000

    feat - add more softwares

--- vps-cpu.sh
+++ vps-cpu.sh
@@ -1,15 +1,35 @@
 sudo apt update
 
 # Install Neo4vim
-sudo apt install neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+sudo rm -rf nvim-linux64.tar.gz
+echo export PATH="$PATH:/opt/nvim-linux64/bin" ~/.bashrc
+
 git clone https://github.com/lpl212757/NVChad ~/.config/nvim
 
+# Install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin -y
+sudo rm -rf lazygit.tar.gz
+sudo rm -rf lazygit
+lazygit --version
+
 # Install ubuntu driver
-sudo apt install ubuntu-drivers-common
+sudo apt install ubuntu-drivers-common -y
+
+# Install NodeJS
+sudo apt update
+sudo apt install nodejs -y
+node -v
+sudo apt install npm -y
 
 # Install python3
-sudo apt install python3
-sudo apt install python3-pip
+sudo apt install python3 -y
+sudo apt install python3-pip -y
 
 # Install conda
 mkdir -p ~/miniconda3
@@ -17,4 +37,20 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 
+# Install Docker
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo systemctl status docker
+
+sudo usermod -aG docker ${USER}
+su - ${USER}
+groups
+sudo usermod -aG docker root
+
+# Start nvim
 nvim
