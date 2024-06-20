7e1058b8b09543351815ac85b4d3cc0ee31f2751
Le Phat Loc <34123125+lpl212757@users.noreply.github.com>
2024-06-21 02:40:56 +0700

    Update vps-cpu.sh

--- vps-cpu.sh
+++ vps-cpu.sh
@@ -5,16 +5,6 @@ sudo git config --global usre.email "lpl212757@gmail.com"
 sudo git config --global user.name "Loc Le"
 sudo git config --global credential.helper store
 
-# Install Neo4vim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-sudo rm -rf nvim-linux64.tar.gz
-echo "export PATH=\"\$PATH:/opt/nvim-linux64/bin\"" >> ~/.bashrc
-export PATH="$PATH:/opt/nvim-linux64/bin"
-
-git clone https://github.com/lpl212757/NVChad ~/.config/nvim
-
 # Install lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -53,13 +43,22 @@ apt-cache policy docker-ce
 sudo DEBIAN_FRONTEND=noninteractive apt install docker-ce -y
 sudo systemctl status docker
 
-sudo usermod -aG docker ${USER}
-su - ${USER}
-groups
-sudo usermod -aG docker root
+# Config permission for Docker
+sudo usermod -a -G docker $USER
+newgrp docker
 
 # Install unzip
 sudo apt install unzip
+
+# Install Neo4vim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+sudo rm -rf nvim-linux64.tar.gz
+echo "export PATH=\"\$PATH:/opt/nvim-linux64/bin\"" >> ~/.bashrc
+export PATH="$PATH:/opt/nvim-linux64/bin"
+
+git clone https://github.com/lpl212757/NVChad ~/.config/nvim
 
 # Start nvim
 nvim
