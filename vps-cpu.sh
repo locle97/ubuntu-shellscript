10bd483b4151cde7b7313a27aa93330392e4a9a9
Le Phat Loc <34123125+lpl212757@users.noreply.github.com>
2024-06-21 02:42:05 +0700

    Update vps-cpu.sh

--- vps-cpu.sh
+++ vps-cpu.sh
@@ -9,7 +9,7 @@ sudo git config --global credential.helper store
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin -y
+sudo install lazygit /usr/local/bin
 sudo rm -rf lazygit.tar.gz
 sudo rm -rf lazygit
 lazygit --version
